1078f8ea2608da56a1ce5e33dda084b6fed7dd1d
nephesh <jmmatthews@proton.me>
2025-03-10 17:42:17 -0500

    Fix tailwind

--- doctor.py
+++ doctor.py
@@ -1,17 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import requests as r
 
-url = 'https://api.fda.gov/drug/label.json?'
-medList = r.get(f'{url}search=openfda.brand_name:"a*"&limit=10')
-useCaseList = r.get(f'{url}count=openfda&limit=1')
+templates = Jinja2Templates(directory='./templates/')
+router = APIRouter()
 
+url = 'https://api.fda.gov/drug/label.json?'
+brandList = r.get(f'{url}search=openfda.brand_name:"a*"&limit=10')
+useCaseList = r.get(f'{url}count=openfda&limit=1')
 
 doctor = FastAPI();
 
+doctor.mount("/static", StaticFiles(directory="static/"), name="static")
+
 @doctor.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
 
-async def root():
-    return {"message": f"{medList.text}"}
-
+@doctor.get("/brands/")
 async def brands():
-    return f"{}"
+    return f"{brandList.text}"
