65ffde000325788d35371c5a5e7f03be01e9fe4d
nephesh <jmmatthews@proton.me>
2025-03-13 14:32:37 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,33 +2,56 @@
   description = "A Nix-flake-based Python development environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nix-github-actions.url = "github:nix-community/nix-github-actions";
+  inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          venvDir = ".venv";
-          packages = with pkgs; [ python313 sqlite basedpyright ] ++
-            (with pkgs.python313Packages; [
-              pip
-              requests
-              python-multipart
-              jinja2
-              sqlmodel
-              fastapi
-              fastapi-cli
-              tailwindcss_4
-              pandas
-              celery
-              venvShellHook
-            ]);
-        };
-      });
+      githubActions = nix-github-actions.lib.mkGithubMatrix { inherit (self) checks; };
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            venvDir = ".venv";
+            packages =
+              with pkgs;
+              [
+                python313
+                sqlite
+                basedpyright
+              ]
+              ++ (with pkgs.python313Packages; [
+                pip
+                requests
+                python-multipart
+                jinja2
+                sqlmodel
+                fastapi
+                fastapi-cli
+                tailwindcss_4
+                pandas
+                celery
+                venvShellHook
+              ]);
+          };
+        }
+      );
     };
 }
