6da01b07c05a7f38ec9e4f033a82775e11c3fc1d
nephesh <jmmatthews@proton.me>
2025-03-13 01:11:41 +0100

    Remove unneeded vars

--- doctor.py
+++ doctor.py
@@ -20,12 +20,6 @@ url = 'https://api.fda.gov/drug/label.json'
 searchTerm = "a*"
 limit = "&limit=10"
 apiOp = "?search="
-brandList = r.get(f'{url}{apiOp}openfda.brand_name:{searchTerm}{limit}')
-genericList = r.get(f'{url}{apiOp}openfda.generic_name:{searchTerm}{limit}')
-manList = r.get(f'{url}{apiOp}openfda.manufacturer_name:{searchTerm}{limit}')
-warnList = r.get(f'{url}{apiOp}boxed_warning:{searchTerm}{limit}')
-dosageList = r.get(f'{url}{apiOp}dosage_and_administration:{searchTerm}{limit}')
-useCaseList = r.get(f'{url}{apiOp}purpose{limit}')
 
 doctor = FastAPI();
 
@@ -53,10 +47,6 @@ SessionDep = Annotated[Session, Depends(get_session)]
 @doctor.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("main.html", {"request": request})
-
-@doctor.get("/brands/")
-async def brands():
-    return f"{brandList.text}"
 
 @doctor.get("/search", response_class=HTMLResponse)
 async def search(request: Request, query: str):
